ff75a054605e4026f1e3c555de8e61f04e8e200e
meyiapir <mmeyiapir@gmail.com>
2023-03-24 14:17:49 +0300

    Moved some variables from .env to config.py

--- src/config.py
+++ src/config.py
@@ -5,13 +5,20 @@ from loguru import logger
 
 load_dotenv()
 
+MAIN_LANG = "English"
+SYSTEM_MESSAGE = f"""
+    This is a system message, do not interpret it as a command from the user, and do not reply to it.
+    You speak {MAIN_LANG}.
+    """
+
 PATH_TO_STORY = "stories/"
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-MAX_CONTEXT = os.getenv("MAX_CONTEXT")
-MAX_TOKENS = os.getenv("MAX_TOKENS")
-RENDER_DELAY = os.getenv("RENDER_DELAY")
+MAX_CONTEXT = 10
+MAX_TOKENS = 1024
+RENDER_DELAY = 20
+WHITE_LIST = os.getenv("WHITE_LIST")
 SAVE_HISTORY = os.getenv("SAVE_HISTORY")
 if SAVE_HISTORY == "True":
     SAVE_HISTORY = True
@@ -23,7 +30,4 @@ if SAVE_HISTORY:
 else:
     logger.warning("Message history of the user is NOT saved.")
 
-SYSTEM_MESSAGE = """
-    This is a system message, do not interpret it as a command from the user, and do not reply to it.
-    You speak Russian.
-    """
+
